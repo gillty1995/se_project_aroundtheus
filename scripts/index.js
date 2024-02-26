82526bc83c479b8675634d1312399a7b2e4fbe27
gillty <gooee@mac.com>
2024-02-26 14:02:16 -0600

    Project 4 Final Stage Submission

--- scripts/index.js
+++ scripts/index.js
@@ -37,12 +37,52 @@ const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
 
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+const cardListEl = document.querySelector(".cards__list");
+
+/* FUNCTIONS */
+
+function closePopup() {
+  profileEditModal.classList.remove("modal__opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  cardTitleEl.textContent = cardData.name;
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  return cardElement;
+}
+
+/* EVENT HANDLERS */
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileTitle.textContent = profileTitleInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+/* EVENT LISTENERS */
+
 profileEditButton.addEventListener("click", () => {
-  profileTitleInput.value = profileTitle.textContent;
-  profileDescriptionInput.value = profileDescription.textContent;
+  profileTitleInput.value = profileTitle.textContent.trim();
+  profileDescriptionInput.value = profileDescription.textContent.trim();
   profileEditModal.classList.add("modal__opened");
 });
 
-profileEditClose.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal__opened");
+profileEditClose.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+/* LOOPS */
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.prepend(cardElement);
 });
