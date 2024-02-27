5a0f8aaaea66a3b1927df9e212a21d8e16c2d319
gillty <gooee@mac.com>
2024-02-26 19:05:28 -0600

    Project 4 Final Stage Fixes

--- scripts/index.js
+++ scripts/index.js
@@ -46,7 +46,7 @@ const cardListEl = document.querySelector(".cards__list");
 /* FUNCTIONS */
 
 function closePopup() {
-  profileEditModal.classList.remove("modal__opened");
+  profileEditModal.classList.remove("modal_opened");
 }
 
 function getCardElement(cardData) {
@@ -73,7 +73,7 @@ function handleProfileEditSubmit(e) {
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent.trim();
   profileDescriptionInput.value = profileDescription.textContent.trim();
-  profileEditModal.classList.add("modal__opened");
+  profileEditModal.classList.add("modal_opened");
 });
 
 profileEditClose.addEventListener("click", closePopup);
